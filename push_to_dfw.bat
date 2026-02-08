5a591d671f68d8b27e486019afc6e93eecc7bf84
kiruthika-arjava <kiruthika33.work@gmail.com>
2026-02-08 21:22:52 +0530

    Cleanup

--- push_to_dfw.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Commit for DFW branch"
-git push https://github.com/h3-services/templewalk_frontend_admin.git master:DFW
--- run_git.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting git process... > git_result.log
-git add . >> git_result.log 2>&1
-echo Done adding. >> git_result.log
-git commit -m "Update code for DFW branch" >> git_result.log 2>&1
-echo Committed. >> git_result.log
-git push https://github.com/h3-services/templewalk_frontend_admin.git master:DFW >> git_result.log 2>&1
-echo Finished push. >> git_result.log
